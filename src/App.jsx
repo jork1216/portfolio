4e4c0b64cd2782b688371037f72595bce9b220bb
junkcarlo16 <ramosjohncarlo16@gmail.com>
2026-03-05 21:59:07 +0800

    made app in desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
 import wallpaper from './images/wallpaper.jpg'
+import myportfolio from './images/myportfolio.png'
 
 function App() {
+  const [showHome, setShowHome] = useState(false)
+
   return (
-    <div className="app-wrapper" style={{ backgroundImage: `url(${wallpaper})` }} >
-      <Home/>
+    <div className="app-wrapper" style={{ backgroundImage: `url(${wallpaper})` }}>
+      <div
+        className={`app-icon-wrapper ${showHome ? 'selected' : ''}`}
+        onClick={() => setShowHome(prev => !prev)}
+      >
+        <img src={myportfolio} alt="My Portfolio" className="app-icon-img" />
+        <span className="app-icon-label">My Portfolio</span>
+      </div>
+
+      {showHome && <Home />}
     </div>
   )
 }
